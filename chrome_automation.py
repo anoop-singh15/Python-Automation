7c214949a41e9b5c982d4735fb12ddc33163fe99
cozytuba <cozytuba1533@gmail.com>
2023-08-13 10:44:31 +0530

    Chrome automation

---
+++ chrome_automation.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+url="172.16.16.16:8090"
+
+chrome_path=r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+webbrowser.register('chrome',None,webbrowser.BackgroundBrowser(chrome_path))
+webbrowser.get('chrome').open_new_tab(url)
